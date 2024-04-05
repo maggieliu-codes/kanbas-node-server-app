f789973887e4f9047bb803a2796a4651de0f4835
maggieliu <magliudh@gmail.com>
2024-04-05 14:26:56 -0400

    finished lab 6

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import express from "express";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
+mongoose.connect("mongodb://localhost:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
